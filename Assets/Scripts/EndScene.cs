ebfd4bf8cb79b5d81064922317541a44766e46ab
Sawmii <sawmiya.stanin@stud.hslu.ch>
2023-11-30 16:12:50 +0100

    EndScene

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -10,7 +10,7 @@ public class EndScene : MonoBehaviour
     {
         if (collision.CompareTag("DwarfKing"))
         {
-            SceneManager.LoadScene("EndGameScreen");
+            SceneManager.LoadScene("WinScreen");
             Debug.Log("detected");
         }
     }
